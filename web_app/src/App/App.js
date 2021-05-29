7153b4edc4517f79114fcf39d8554b27891b2c12
Gert Nel <21567042@sun.ac.za>
2021-05-29 11:58:40 +0200

    Updated some stuff

--- web_app/src/App/App.js
+++ web_app/src/App/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Login from '../Login/login';
-import NotFound from '../NotFound/NotFound'
+import NotFound from '../NotFound/NotFound';
+import Home from '../Home/Home';
+
 
 
 class App extends React.Component {
     render() { return(
         <BrowserRouter>
             <Switch>
-            <Route exact path="/">
+            <Route path="/login">
                 <Login />
             </Route>
+            <Route path ='/' component={Home} />
             <Route component={NotFound} />
             </Switch>
         </BrowserRouter>);
--- web_app/src/Login/login.js
+++ web_app/src/Login/login.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import LoginBox from './loginBox';
+import RegisterBox from './registerBox';
+import './login.css';
+//import { useSpring, animated } from "react-spring";
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          loginNotRegister : true
+        };
+      }
+
     render() {
+        let re;
+        if(this.state.loginNotRegister){
+            re = <LoginBox/>;
+        }
+        else{
+            re = <RegisterBox />;
+        }
         return(
-            <div className="loginPage">
-                <h1>Login</h1>
+            <div className="box">
+                <img class="watermark" src={'./logo1.png'} alt="Logo"/>
+                {re}
             </div>
-        );
+            );
       }
 }
 
