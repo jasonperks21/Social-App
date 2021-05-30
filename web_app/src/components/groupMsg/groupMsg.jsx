ba9ddd5677cebce2293cfc1e38ad951ff417e404
Gert Nel <21567042@sun.ac.za>
2021-05-30 22:59:08 +0200

    Check replies on group messages

--- web_app/src/components/groupMsg/groupMsg.jsx
+++ web_app/src/components/groupMsg/groupMsg.jsx
@@ -1,8 +1,7 @@
-import "../post/post.css";
+import "./groupMsg.css";
 import TimeAgo from 'react-timeago';
 
 export default function GroupMsg(message) {
-    let bool = false;
     const msg = message.msg; 
   return (
     <div className="post">
@@ -17,22 +16,37 @@ export default function GroupMsg(message) {
           <span className="postText">{msg.message}</span>
         </div>
         <div className="postBottom">       
-            {checkReply(msg.replies.length, bool, msg.replies)}
+            {checkReply(msg.replies)}
         </div>
       </div>
     </div>
   );
 }
 
-function checkReply(num, bool, replies){
-    if(bool){
-        return <p>text</p>;
-    }
+function checkReply(replies){
+    let num = replies.length
     if(num>0){
-        return (<div className="postBottomRight"><img src="/assets/comment.png" alt="" className="commentIcon" />
-                <span className="postCommentText">{num} Replies</span></div>);
-    }
+        return <div>{displayReply(replies)}</div>;
+    } 
+
     else{
         return <div></div>;
     }
+}
+
+function displayReply(replies){
+    if(replies !== undefined){
+        if(replies.length>0){
+            let obj = [];
+            replies.forEach(element => {
+                obj.push(<GroupMsg msg={element} />);
+                if(element.replies>0){
+                    displayReply(element.replies);
+                }
+            });
+            return <div className="PostBottomRight">{obj} </div>;
+        }
+        
+    }
+    return <div></div>;
 }
