190ed5422912b57bf9f665d7663683f0212a8ff2
Gert Nel <21567042@sun.ac.za>
2021-05-31 16:14:07 +0200

    Added collapseable messages and reply boxes

--- web_app/src/components/groupMsg/groupMsg.jsx
+++ web_app/src/components/groupMsg/groupMsg.jsx
@@ -1,8 +1,56 @@
 import "./groupMsg.css";
+import React from 'react';
 import TimeAgo from 'react-timeago';
 
-export default function GroupMsg(message) {
-    const msg = message.msg; 
+class GroupMsg extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        msg: this.props.msg,
+        expand: false,
+        setMsg: null
+
+    }
+    this.clicked = this.clicked.bind(this);
+    this.post = this.post.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+}
+handleChange(event) {
+  this.setState({setMsg: event.target.value});
+}
+  clicked(){
+    this.setState({expand: true});
+  }
+  post(){
+    alert(this.state.setMsg);
+    this.setState({setMsg: ''});
+  }
+  reply(){
+    return (<div className="postBottomRight" ><input className="replyMsg" type='text' value={this.state.setMsg} onChange={this.handleChange} placeholder="Reply Message"></input>
+    <button className="sendIcon" onClick={()=> this.post()}>Post</button>  
+    </div>);
+  }
+
+  expand(){
+    if(this.state.expand){
+      return <div> {checkReply(this.state.msg.replies, this.state.expand)}{this.reply()}</div>;
+    }
+    let num = this.state.msg.replies.length;
+    if(num > 0){
+      let text = "1 Reply";
+      if(num>1){
+        text = String(num)+" Replies"
+      }
+      return (<div className = "postBottomRight">
+        <img src="/assets/comment.png" alt="" className="commentIcon" />
+        <button className="postCommentText" onClick={() => this.clicked()}> {text} </button>
+      </div>);
+    }
+   return this.reply();
+  }
+
+  render(){
+    const msg = this.state.msg; 
   return (
     <div className="post">
       <div className="postWrapper">
@@ -16,12 +64,16 @@ export default function GroupMsg(message) {
           <span className="postText">{msg.message}</span>
         </div>
         <div className="postBottom">       
-            {checkReply(msg.replies)}
+            {this.expand()}
         </div>
+        
       </div>
     </div>
   );
+  }
 }
+
+export default GroupMsg;
 
 function checkReply(replies){
     let num = replies.length
